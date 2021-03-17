1f0096f3dd351ed933a92486e5fc0a8ef000ed56
Yaping Liu <yaping.liu12@outlook.com>
2021-03-16 19:43:05 -0700

    updated the solution for task 3

--- index.js
+++ index.js
@@ -96,9 +96,12 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby(attr) {
-   Person.call(this,attr);
-   this.favoriteToy = attr.favoriteToy;
+ function Baby(name,age,favoriteToy) {
+   Person.call(this,name,age);
+   this.favoriteToy = favoriteToy;
+    // this.name = name;
+    // this.age = age;
+    // this.favoriteToy = favoriteToy;
   }
 
   Baby.prototype = Object.create(Person.prototype);
